c628e90961226b887e1208aa582eb7a34a75e23f
AKram khan <akramkhangkp09@gmail.com>
2021-10-20 17:07:40 +0530

    ar

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -121,8 +121,7 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/portfolio/static/',
+    os.path.join(BASE_DIR,'static')
 ]
 MEDIA_URL='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
